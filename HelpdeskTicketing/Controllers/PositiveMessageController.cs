cb00058ebd05ed9d3075f615ed195b0352b9646a
Lori Woodward <lannstoirme@Loris-Air.lan>
2020-09-20 12:24:10 +1000

    Linq queries added

---
+++ HelpdeskTicketing/Controllers/PositiveMessageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HelpdeskTicketing.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpdeskTicketing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class PositiveMessageController : ControllerBase
+    {
+        private readonly PositiveMessageService _positivemessageService;
+
+        public PositiveMessageController(PositiveMessageService positivemessageService)
+        {
+
+            _positivemessageService = positivemessageService;
+            
+        }
+
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { };
+        }
+        //public ActionResult Get()
+        //{
+        //    var positivemessage = _positivemessageService.Get();
+        //
+        //    if (positivemessage == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+          //  return (ActionResult)positivemessage;
+        //}
+
+    }
+}
--- HelpdeskTicketing/Services/PositiveMessageService.cs
+++ HelpdeskTicketing/Services/PositiveMessageService.cs
@@ -13,41 +13,63 @@ using System.Collections;
 
 namespace HelpdeskTicketing.Services
 {
-    public class PositiveMessageService
+    public class PositiveMessageService : IEnumerable<string>
     {
         private readonly IMongoCollection<Message> _messages;
         private readonly IMongoCollection<User> _users;
         private readonly MessageService _messageService;
         private readonly UserService _userService;
 
-        //public List<String> positivemessageslist = new List<String>();
+        public List<string> positivemessages;
 
-        public PositiveMessageService(IHelpdeskDatabaseSettings settings)
+        
+
+        public PositiveMessageService(IHelpdeskDatabaseSettings settings, string[] positivemessages, MessageService _messageService, UserService _userService)
         {
-            var client = new MongoClient(settings.ConnectionString);
-            var database = client.GetDatabase(settings.DatabaseName);
+            //var client = new MongoClient(settings.ConnectionString);
+            //var database = client.GetDatabase(settings.DatabaseName);
 
-            _messages = database.GetCollection<Message>(settings.MessageCollectionsName);
-            _users = database.GetCollection<User>(settings.UserCollectionsName);
+            //_messages = database.GetCollection<Message>(settings.MessageCollectionsName);
+            //_users = database.GetCollection<User>(settings.UserCollectionsName);
+
+            //this.positivemessages = new List<string>(positivemessages);
+
+            //var positivemessages = from message in _messageService.Get()
+            //                       join user in _userService.Get() on message.Id equals user.Id into userPositiveMessageGroup
+            //                       where message.SentimentApplied == true && message.Sentiment == "positive"
+            //                       select userPositiveMessageGroup.ToList();
+
+            var client = new MongoClient("mongodb+srv://lannstoirme:#####@cluster0.nwr3l.azure.mongodb.net/AsgardiaDB?retryWrites=true&w=majority");
+            var database = client.GetDatabase("AsgardiaDB");
+            var collection = database.GetCollection<Message>("Message");
+            var collection2 = database.GetCollection<User>("UserData");
+
+            //var query =
+            //    from p in collection.AsQueryable<Message>()
+            //    where p.Sentiment == "positive"
+            //    select p;
+
+            var innerJoinQuery =
+                from p in collection.AsQueryable<Message>()
+                join p2 in collection2.AsQueryable<User>() on p.UserId equals p2.DiscordId
+                where p.Sentiment == "positive"
+                select new { UnId = p2.Id, FName = p2.FirstName, LName = p2.LastName, MessageContent = p.MessageText };
+
+            //foreach (var message in query)
+            //{
+            //    Console.WriteLine(message.MessageText);
+            //}
+
+            foreach (var message in innerJoinQuery)
+            {
+                Console.WriteLine("Unique Identifier is: " + message.UnId);
+                Console.WriteLine("Name is: " + message.FName + ' ' + message.LName);
+                Console.WriteLine("Message content is: " + message.MessageContent);
+
+                //public List<AsyncResult> result = new List<AsyncResult>();
+
+
+            }
+
+
         }
-
-
-        public List<string> PositiveMessageFilter(MessageService _messageService, UserService _userService)
-        {
-
-
-            var positivemessages = from message in _messageService.Get()
-                                   join user in _userService.Get() on message.Id equals user.Id into userPositiveMessageGroup
-                                   where message.SentimentApplied == true && message.Sentiment == "positive"
-                                   select userPositiveMessageGroup;
-
-     
-
-            return (List<string>)positivemessages;
-
-        }
-
-       
-
-}
-}
